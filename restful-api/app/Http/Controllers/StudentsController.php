e024890454554751c986b0ec6d37c60af3a2be77
LIan2nd <alfiannurusyaid19@gmail.com>
2023-10-27 09:27:45 +0700

    Praktikum dengan eloquent (Tugas belum kelar)

---
+++ restful-api/app/Http/Controllers/StudentsController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+
+        $data = [
+            'message' => 'Get all students',
+            'data' => $students
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = [
+            'name' => $request->name,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ];
+
+        $student = Student::create($input);
+
+        $data = [
+            'message' => 'Student is created succesfully',
+            'data' => $student
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
